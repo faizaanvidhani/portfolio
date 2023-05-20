103a7bed396da4f6f4ea55ed57527dae2e5eb3b0
Faizaan Vidhani <faizaanvidhani@gmail.com>
2023-05-20 17:14:00 -0400

    modified home

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -4,25 +4,19 @@ export default function HomeBody() {
         <div className="home-body">
             <p className="home-body-text">
             Hi! I'm Faizaan (he/him/his). I’m a Dallas native with a passion for health technology, computer systems, and 
-            clinical applications of AI. I attend <a className="home-link" href="https://www.brown.edu">Brown University</a>, 
-            where I study <span className="home-important">neuroscience</span> and <span className="home-important">computer science</span> with 
-            a focus in <span className="home-important">visual computing</span>. 
+            clinical applications of AI. I graduated from <a className="home-link" href="https://www.brown.edu">Brown University</a> with concentrations
+            in <span className="home-important">neuroscience</span> and <span className="home-important">computer science</span>. 
             </p>
+           
             <p className="home-body-text">
-            I have a diverse background in both industry and research. In the summer of 2022, I completed a <span className="home-important">Software Engineering</span> internship at&nbsp;
-            <a className="home-link" href="https://www.amazon.com">Amazon</a>, where I worked on the Access Engineering to design and deploy a KillSwitch to production.
-            During my undergraduate years, I conducted both dry lab and wet lab research. In my first year, I had the opportunity to contribute to <span className="home-important">epilepsy research</span> at&nbsp;
-            <a className="home-link" href="https://www.brown.edu/Research/asaad/">Rhode Island Hospital</a>, where I delved into the neural 
-            processes involved in facial recognition by contributing to a movie-viewing task. During the summer after my sophomore year, I 
-            conducted <span className="home-important">autism genetics research</span> at <a className="home-link" href="https://chahrourlab.org/index.html#home">UT Southwestern Medical Center</a>. 
+            I love tackling challenging problems in both the <span className="home-important">health</span> and <span className="home-important">tech</span> space.
+            I am specifically passionate about leveraging computational tools and methods to reduce inefficiencies and improve patient outcomes.
             </p>
+
             <p className="home-body-text">
-            I love tackling challenging problems in both the <span className="home-important">health</span> and <span className="home-important">tech</span> space. 
-            Currently, I’m seeking opportunities at the intersection of these two fields.
+            If you're looking to connect, let's chat via <a className="home-important" href="mailto:faizaanvidhani@gmail.com">email</a>! 
             </p>
-            <p className="home-body-text">
-            I am using this website to display some of the things I’ve been working on. If you're looking to connect, let's chat via <a className="home-important" href="mailto:faizaanvidhani@gmail.com">email</a>. 
-            </p>
+
         </div>
     )
 }
